1d1da9ecda1adb3b70567a399eacd9bda1e6b645
Aaron Klump <sourcecode@intheloftstudios.com>
2024-07-03 17:02:45 -0700

    Update test runner.

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
+#!/USR/BIN/ENV BASH
 s="${BASH_SOURCE[0]}";[[ "$s" ]] || s="${(%):-%N}";while [ -h "$s" ];do d="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$d/$s";done;__DIR__=$(cd -P "$(dirname "$s")" && pwd)
 
 cd "$__DIR__/.."
 
+# https://phpunit.readthedocs.io/en/9.5/textui.html#command-line-options
 ./vendor/bin/phpunit -c tests_unit/phpunit.xml "$@"
-exit $?
+#./vendor/bin/phpunit -c tests_unit/phpunit.xml --testdox "$@"
+#export XDEBUG_MODE=$XDEBUG_MODE,coverage;./vendor/bin/phpunit -c tests_unit/phpunit.xml "$@" --coverage-html=./tests_unit/reports
+#echo ./tests_unit/reports/index.html
