511c31f85e83527470c00b5f5c0747c88610eb0b
Aaron Klump <sourcecode@intheloftstudios.com>
2026-07-20 17:04:40 -0700

    chore: Remove unused Bash scripts and associated unit tests. Cleanup for release
    Remove deprecated Bash scripts (`bind_book.sh`, `run_multi_php_unit_tests.sh`, `run_unit_tests.sh`) and their respective unit tests from the `tests_unit` directory, including `BashTest`, `ColorTest`, `ConfigurationTest`, `LegacyConfigurationTest`, `OutputTest`, and `ArraysTest`.

--- .web_package/hooks/build/00_docs.sh
+++ .web_package/hooks/build/00_docs.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-
 # @file Compile documentation using Knowledge
 
-bin/bind_book.sh
+# ========= Start config =========
+php_dir="$(find /Applications/MAMP/bin/php -maxdepth 1 -type d -name 'php7.4.*' | sort -V | tail -n 1)"
+php="$php_dir/bin/php"
+know="$HOME/Code/Packages/php/knowledge/app/bin/book.php"
+# ========= End config =========
+
+if [[ ! -x "$php" || ! -f "$know" ]]; then
+  echo 'You must install https://github.com/aklump/knowledge to compile documentation' && exit 1
+fi
+
+if ! "$php" "$know" bind ./knowledge; then
+  echo 'You must install https://github.com/aklump/knowledge to compile documentation' && exit 1
+fi
---
+++ todo.md
@@ -0,0 +1,9 @@
+## Blockers
+
+## Critical
+
+## Normal
+
+## Backlog
+
+## Notes
